8343fed5717bb3b5c11eba94b05621c14612a0b4
Chris Bush <chris.bush@mongodb.com>
2021-03-10 17:40:58 -0500

    Improve loading and docs on WelcomeViewController

--- ios-swift/App/WelcomeViewController.swift
+++ ios-swift/App/WelcomeViewController.swift
@@ -5,21 +5,21 @@ class WelcomeViewController: UIViewController {
     @IBOutlet weak var emailField: UITextField!
     @IBOutlet weak var passwordField: UITextField!
     @IBOutlet weak var submitButton: UIButton!
-    @IBOutlet weak var modeChangeButton: UIButton!
+    @IBOutlet weak var changeModeButton: UIButton!
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
 
     private var isInSignUpMode = true
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        modeChangeButton.addTarget(self, action: #selector(modeChangeButtonPressed), for: .touchUpInside)
+        changeModeButton.addTarget(self, action: #selector(modeChangeButtonPressed), for: .touchUpInside)
         submitButton.addTarget(self, action: #selector(submitButtonPressed), for: .touchUpInside)
         // If a user is already looged in, go directly to the items screen
         if realmApp.currentUser != nil {
             self.performSegue(withIdentifier: "onAuthenticationComplete", sender: self)
         }
     }
-    
+
     override func viewWillAppear(_ animated: Bool) {
         navigationController?.navigationBar.isHidden = true
     }
@@ -28,17 +28,19 @@ class WelcomeViewController: UIViewController {
         navigationController?.navigationBar.isHidden = false
     }
 
+    /// Switches the UI between sign in (log in) and sign up (create account) mode.
     @IBAction func modeChangeButtonPressed() {
         isInSignUpMode = !isInSignUpMode
         if (isInSignUpMode) {
             submitButton.setTitle("Create Account", for: .normal)
-            modeChangeButton.setTitle("Already have an account? Log In", for: .normal)
+            changeModeButton.setTitle("Already have an account? Log In", for: .normal)
         } else {
             submitButton.setTitle("Log In", for: .normal)
-            modeChangeButton.setTitle("Don't have an account? Create Account", for: .normal)
+            changeModeButton.setTitle("Don't have an account? Create Account", for: .normal)
         }
     }
     
+    /// Action called when the submit button is pressed to either create an account or log in.
     @IBAction func submitButtonPressed() {
         let email = emailField.text!
         let password = passwordField.text!
@@ -49,9 +51,12 @@ class WelcomeViewController: UIViewController {
         }
     }
     
+    /// Registers a new user with the email/password authentication provider.
     func signUp(email: String, password: String) {
+        setLoading(true)
         realmApp.emailPasswordAuth.registerUser(email: email, password: password) { error in
             DispatchQueue.main.async {
+                self.setLoading(false)
                 guard error == nil else {
                     print("Account creation failed: \(error!)")
                     self.reportError(error!)
@@ -62,11 +67,12 @@ class WelcomeViewController: UIViewController {
         }
     }
     
+    /// Logs in with an existing user.
     func signIn(email: String, password: String) {
-        activityIndicator.startAnimating()
+        setLoading(true)
         realmApp.login(credentials: Credentials.emailPassword(email: email, password: password)) { result in
             DispatchQueue.main.async {
-                self.activityIndicator.stopAnimating()
+                self.setLoading(false)
                 switch result {
                 case let .failure(error):
                     print("Failed to log in: \(error)")
@@ -78,13 +84,14 @@ class WelcomeViewController: UIViewController {
         }
     }
     
+    /// Called when sign in completes. Opens the realm asynchronously and navigates to the Tasks screen.
     func onSignInComplete(_ user: User) {
         Realm.Configuration.defaultConfiguration = user.configuration(partitionValue: user.id)
 
-        activityIndicator.startAnimating()
+        setLoading(true)
         Realm.asyncOpen { result in
             DispatchQueue.main.async {
-                self.activityIndicator.stopAnimating()
+                self.setLoading(false)
                 switch result {
                 case  let .failure(error):
                     print("Failed to open realm: \(error)")
@@ -97,6 +104,7 @@ class WelcomeViewController: UIViewController {
         }
     }
     
+    /// Presents the user with an error alert.
     func reportError(_ error: Error) {
         let alertController = UIAlertController(title: "Error", message: error.localizedDescription, preferredStyle: .alert)
         alertController.addAction(UIAlertAction(title: "Ok", style: .default, handler: { _ in
@@ -104,4 +112,18 @@ class WelcomeViewController: UIViewController {
         }))
         present(alertController, animated: true)
     }
+
+    /// Turns on or off the activity indicator.
+    func setLoading(_ loading: Bool) {
+        if loading {
+            activityIndicator.startAnimating()
+        } else {
+            activityIndicator.stopAnimating()
+        }
+        emailField.isEnabled = !loading
+        passwordField.isEnabled = !loading
+        submitButton.isEnabled = !loading
+        changeModeButton.isEnabled = !loading
+    }
+
 }
