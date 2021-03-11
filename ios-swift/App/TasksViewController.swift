7f82b6c71ea362355caaa8d472ab2652723b8338
Chris Bush <chris.bush@mongodb.com>
2021-03-11 15:42:03 -0500

    Implement add task

--- ios-swift/App/TasksViewController.swift
+++ ios-swift/App/TasksViewController.swift
@@ -1,13 +1,22 @@
 import UIKit
+import RealmSwift
 
 class TasksViewController: UITableViewController {
+    @IBOutlet var logOutBarButtonItem: UIBarButtonItem!
+    @IBOutlet var addTaskBarButtonItem: UIBarButtonItem!
+
+    var realm: Realm!
+
     override func viewDidLoad() {
-        // On the top left is a log out button.
-        navigationController?.navigationBar.isHidden = false
-        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Log Out", style: .plain, target: self, action: #selector(logOutButtonDidClick))
+        logOutBarButtonItem.target = self
+        logOutBarButtonItem.action = #selector(logOut)
+        addTaskBarButtonItem.target = self
+        addTaskBarButtonItem.action = #selector(addTask)
+        
+        realm = try! Realm()
     }
 
-    @objc func logOutButtonDidClick() {
+    @objc func logOut() {
         let alertController = UIAlertController(title: "Log Out", message: nil, preferredStyle: .alert)
         alertController.addAction(UIAlertAction(title: "Yes, Log Out", style: .destructive) { _ in
             print("Logging out...")
@@ -20,5 +29,30 @@ class TasksViewController: UITableViewController {
         })
         alertController.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
         present(alertController, animated: true, completion: nil)
+    }
+    
+    @objc func addTask() {
+        let alertController = UIAlertController(title: "Add Task", message: nil, preferredStyle: .alert)
+
+        // When the user clicks the add button, present them with a dialog to enter the task name.
+        alertController.addAction(UIAlertAction(title: "Save", style: .default, handler: { _ in
+            let textField = alertController.textFields![0] as UITextField
+
+            // Create a new Task with the text that the user entered.
+            let task = Task(summary: textField.text ?? "New Task")
+
+            // Any writes to the Realm must occur in a write block.
+            try! self.realm.write {
+                // Add the Task to the Realm. That's it!
+                self.realm.add(task)
+            }
+        }))
+        alertController.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        alertController.addTextField(configurationHandler: { (textField: UITextField!) -> Void in
+            textField.placeholder = "New Task Summary"
+        })
+
+        // Show the dialog.
+        self.present(alertController, animated: true, completion: nil)
     }
 }
