da4c6a2b400d57b7a37cc963db5787364106716d
Jennifer Peshansky <jennifer.peshansky@mongodb.com>
2021-03-22 13:39:09 -0400

    Initial commit

---
+++ android-kotlin/app/src/main/java/com/mongodb/app/ui/login/LoginResult.kt
@@ -0,0 +1,9 @@
+package com.mongodb.app.ui.login
+
+/**
+ * Authentication result : success (user details) or error message.
+ */
+data class LoginResult(
+        val success: LoggedInUserView? = null,
+        val error: Int? = null
+)
