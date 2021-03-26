2235d723f6dc70f1ff0f04a84bd9c2933eae75be
Jennifer Peshansky <54945270+jenniferpeshansky@users.noreply.github.com>
2021-03-26 11:31:21 -0400

    DOCSP-14516 Android - Implement welcome screen (#5)
    * First commit
    
    * Initial commit
    
    * revert .gitignore
    
    * Cleaned up login activity and created skeleton items activity
    
    * some changes
    
    * functioning welcome screen
    
    * removed action bar from login screen
    
    * fixed .gitignore
    
    * added newlines to end of files
    
    * cleaned up unnecessary files
    
    * addressed code review comments
    
    * changed main screen
    
    * addressed comments

---
+++ android-kotlin/app/src/main/java/com/mongodb/app/RealmApp.kt
@@ -0,0 +1,37 @@
+package com.mongodb.app
+
+import android.app.Application
+import android.util.Log
+
+import io.realm.Realm
+import io.realm.log.LogLevel
+import io.realm.log.RealmLog
+import io.realm.mongodb.App
+import io.realm.mongodb.AppConfiguration
+
+lateinit var realmApp: App
+
+// global Kotlin extension that resolves to the short version
+// of the name of the current class. Used for labelling logs.
+inline fun <reified T> T.TAG(): String = T::class.java.simpleName
+
+/*
+* TemplateApp: Sets up the template Realm App and enables Realm-specific logging in debug mode.
+*/
+class TemplateApp: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        Realm.init(this)
+         realmApp = App(
+            AppConfiguration.Builder(getString(R.string.mongodb_realm_app_id))
+                .build())
+
+        // Enable more logging in debug mode
+        if (BuildConfig.DEBUG) {
+            RealmLog.setLevel(LogLevel.ALL)
+        }
+
+        Log.v(TAG(), "Initialized the Realm App configuration for: ${realmApp.configuration.appId}")
+    }
+}
