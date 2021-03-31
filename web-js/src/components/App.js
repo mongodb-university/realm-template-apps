eb747f3766fb10cfc4b372dc5a8e8bdac665d699
Nick Larew <nick.larew@mongodb.com>
2021-03-30 21:02:10 -0400

    (DOCSP-14525): Web (GraphQL & Remote MongoDB) - Create project with Realm dependencies

---
+++ web-js/src/components/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+// <img src={logo} className="App-logo" alt="logo" />
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+export default App;
