cce3df83e7dcc4b4575b4112db34bac1b608390e
Jennifer Peshansky <jennifer.peshansky@mongodb.com>
2021-04-06 10:39:14 -0500

    refactored to pass config + some cleanup

--- android-kotlin/app/src/main/java/com/mongodb/app/ItemAdapter.kt
+++ android-kotlin/app/src/main/java/com/mongodb/app/ItemAdapter.kt
@@ -6,16 +6,22 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import io.realm.OrderedRealmCollection
 import io.realm.Realm
+import io.realm.RealmConfiguration
 import io.realm.RealmRecyclerViewAdapter
 import io.realm.kotlin.where
-import io.realm.mongodb.sync.SyncConfiguration
 import org.bson.types.ObjectId
 
 /**
  * Extends the Realm-provided RealmRecyclerViewAdapter to provide data
  * for a RecyclerView to display Realm objects on screen to a user.
  */
-internal class ItemAdapter(data: OrderedRealmCollection<Item>, val user: io.realm.mongodb.User, private val partition: String) : RealmRecyclerViewAdapter<Item, ItemAdapter.ItemViewHolder?>(data, true) {
+internal class ItemAdapter(data: OrderedRealmCollection<Item>, private val config: RealmConfiguration) : RealmRecyclerViewAdapter<Item, ItemAdapter.ItemViewHolder?>(data, true) {
+
+    internal inner class ItemViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        var id: ObjectId? = null
+        var name: TextView = view.findViewById(R.id.name)
+        var menu: TextView = view.findViewById(R.id.popup_menu)
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
         val itemView: View = LayoutInflater.from(parent.context).inflate(R.layout.item_view, parent, false)
@@ -27,41 +33,25 @@ internal class ItemAdapter(data: OrderedRealmCollection<Item>, val user: io.real
         holder.id = obj?._id
         holder.name.text = obj?.name
 
+        /**
+         *  Creates a popup menu that allows the user to delete an item from the realm.
+         */
         holder.itemView.setOnClickListener {
             val popup = PopupMenu(holder.itemView.context, holder.menu)
-            val menu = popup.menu
-            val deleteCode = -1
-            menu.add(0, deleteCode, Menu.NONE, "Delete")
-            popup.setOnMenuItemClickListener { menuItem: MenuItem? ->
-                when (menuItem!!.itemId) {
-                    deleteCode -> {
-                        deleteItem(holder.id!!)
-                    }
+            popup.menu.add(0, R.id.action_delete, Menu.NONE, "Delete")
+            popup.setOnMenuItemClickListener { menuItem: MenuItem ->
+                when (menuItem.itemId) {
+                    R.id.action_delete -> {
+                        val realm: Realm = Realm.getInstance(config)
+                        realm.executeTransactionAsync {
+                            val item = it.where<Item>().equalTo("_id", holder.id).findFirst()
+                            item?.deleteFromRealm()
+                        }
+                        realm.close()                    }
                 }
                 true
             }
             popup.show()
         }
     }
-
-    /**
-     *  Allows the user to delete an item from the realm.
-     */
-    private fun deleteItem(id: ObjectId) {
-        val config = SyncConfiguration.Builder(user, partition).build()
-
-        val realm: Realm = Realm.getInstance(config)
-        realm.executeTransactionAsync {
-            val item = it.where<Item>().equalTo("_id", id).findFirst()
-            item?.deleteFromRealm()
-        }
-        realm.close()
-    }
-
-    internal inner class ItemViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        var name: TextView = view.findViewById(R.id.name)
-        var id: ObjectId? = null
-        var menu: TextView = view.findViewById(R.id.menu)
-    }
-
 }
