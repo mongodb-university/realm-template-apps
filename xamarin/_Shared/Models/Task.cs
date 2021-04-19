68ea2fba50d77189f415bcab30bf142442145011
MongoCaleb <32645888+MongoCaleb@users.noreply.github.com>
2021-04-19 11:16:23 -0700

    Xamarin (C#) Template App (#6)
    Template App for Xamarin

---
+++ xamarin/_Shared/Models/Task.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using MongoDB.Bson;
+using Realms;
+
+namespace RealmTemplateApp.Models
+{
+    public class Task : RealmObject
+    {
+        [PrimaryKey]
+        [MapTo("_id")]
+        [Required]
+        public string Id { get; set; } = ObjectId.GenerateNewId().ToString();
+
+        [MapTo("_partition")]
+        [Required]
+        public string Partition { get; set; }
+
+        [MapTo("summary")]
+        [Required]
+        public string Summary { get; set; }
+
+        [MapTo("isComplete")]
+        public bool IsComplete { get; set; }
+    }
+}
