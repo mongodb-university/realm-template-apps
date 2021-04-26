38f53885c9f532fcd30f663e005dca52e9aa3160
Nick Larew <nick.larew@mongodb.com>
2021-04-26 12:27:51 -0400

    Auto-login & better auth errors

--- web-js/src/components/WelcomePage.js
+++ web-js/src/components/WelcomePage.js
@@ -39,9 +39,8 @@ export function WelcomePage() {
     try {
       if (isSignup) {
         await realmApp.emailPasswordAuth.registerUser(email, password);
-      } else {
-        await realmApp.logIn(Realm.Credentials.emailPassword(email, password));
       }
+      await realmApp.logIn(Realm.Credentials.emailPassword(email, password));
     } catch (err) {
       handleAuthenticationError(err, setError);
     }
@@ -116,34 +115,24 @@ export function WelcomePage() {
   );
 }
 
-function parseAuthenticationError(err) {
-  const parts = err.message.split(":");
-  const reason = parts[parts.length - 1].trimStart();
-  if (!reason) return { status: "", message: "" };
-  const reasonRegex = /(?<message>.+)\s\(status (?<status>[0-9][0-9][0-9])/;
-  const match = reason.match(reasonRegex);
-  const { status, message } = match?.groups ?? {};
-  return { status, message };
-}
-
 function handleAuthenticationError(err, setError) {
-  const { status, message } = parseAuthenticationError(err);
-  const errorType = message || status;
+  const { error, statusCode } = err
+  const errorType = error || statusCode;
   switch (errorType) {
     case "invalid username":
       setError((prevErr) => ({ ...prevErr, email: "Invalid email address." }));
       break;
     case "invalid username/password":
     case "invalid password":
-    case "401":
+    case 401:
       setError((err) => ({ ...err, password: "Incorrect password." }));
       break;
     case "name already in use":
-    case "409":
+    case 409:
       setError((err) => ({ ...err, email: "Email is already registered." }));
       break;
     case "password must be between 6 and 128 characters":
-    case "400":
+    case 400:
       setError((err) => ({
         ...err,
         password: "Password must be between 6 and 128 characters.",
