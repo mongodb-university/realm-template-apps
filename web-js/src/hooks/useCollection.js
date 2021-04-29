48fcf3de3f71ad1f351b15b8f08a804bbbd4d8df
Nick Larew <nick.larew@mongodb.com>
2021-04-29 03:47:56 -0400

    Add more type hints

--- web-js/src/hooks/useCollection.js
+++ web-js/src/hooks/useCollection.js
@@ -3,10 +3,12 @@ import { useRealmApp } from "../components/RealmApp";
 
 /**
  * Returns a MongoDB Collection client object
+ * @template DocType extends Realm.Services.MongoDB
  * @param {Object} config - A description of the collection.
- * @param {string} config.service - The service name of the collection's linked cluster.
+ * @param {string} [config.service="mongodb-atlas"] - The service name of the collection's linked cluster.
  * @param {string} config.db - The name of database that contains the collection.
  * @param {string} config.collection - The name of the collection.
+ * @returns {Realm.Services.MongoDB.MongoDBCollection<DocType>} config.collection - The name of the collection.
  */
 export function useCollection({ service="mongodb-atlas", db, collection }) {
   const realmApp = useRealmApp();
