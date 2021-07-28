3f9f1b88584625f90724c9dcfa48a58b6584399c
Chris Bush <chris.bush@10gen.com>
2021-07-28 16:07:37 -0400

    Use @Persisted property wrappers in Swift template app (#34)

--- ios-swift/App/Task.swift
+++ ios-swift/App/Task.swift
@@ -2,13 +2,10 @@ import Foundation
 import RealmSwift
 
 class Task: Object {
-    @objc dynamic var _id: ObjectId = ObjectId.generate()
-    @objc dynamic var isComplete: Bool = false
-    @objc dynamic var summary: String = ""
-    override static func primaryKey() -> String? {
-        return "_id"
-    }
-    
+    @Persisted(primaryKey: true) var _id: ObjectId
+    @Persisted var isComplete = false
+    @Persisted var summary = ""
+
     convenience init(summary: String) {
         self.init()
         self.summary = summary
