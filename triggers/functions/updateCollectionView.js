224a5d3147bd385399f2f4ca043e20cd25c9509c
MongoCaleb <32645888+MongoCaleb@users.noreply.github.com>
2021-08-09 12:31:22 -0700

    two small fixes in intro text (#36)

--- triggers/functions/updateCollectionView.js
+++ triggers/functions/updateCollectionView.js
@@ -1,12 +1,13 @@
-exports = function(changeEvent) {
+exports = function (changeEvent) {
   /*
-    This database trigger copies the inserted document into another collection and records the time at which it was created to create a different view.
+    This database trigger copies the inserted document into another collection 
+    and records the time at which it was created to create a different view.
     
     Test it out:
-    1. Go to www.atlas.mongodb.com/<project_id>/dataexplorer
+    1. Go to cloud.mongodb.com, log on, and click the Browse Collections button 
     2. Add a database called 'db' and a collection called 'items'
     3. Insert a document in the 'items' collection
-    3. Noice that the document in 'items_copy' collection has been inserted and updated
+    3. Notice that the document in 'items_copy' collection has been inserted and updated
   */
   const mongodb = context.services.get("mongodb-atlas");
   const db = mongodb.db("db");
@@ -16,15 +17,15 @@ exports = function(changeEvent) {
   if (changeEvent.operationType === "insert") {
     const date = new Date();
     var fullDocument = changeEvent.fullDocument;
-    fullDocument = { ...fullDocument, date_created: date};
-    
+    fullDocument = { ...fullDocument, date_created: date };
+
     postsCopyCollection.insertOne(fullDocument)
-    .then(result => console.log(`Successfully inserted item with _id: ${result.insertedId}`))
-    .catch(err => console.error(`Failed to insert item: ${err}`))
-    return; 
+      .then(result => console.log(`Successfully inserted item with _id: ${result.insertedId}`))
+      .catch(err => console.error(`Failed to insert item: ${err}`))
+    return;
   } else {
     console.log("Unexpected database action detected. Document was not copied over.")
     return;
   }
-  
+
 };
