35d4077e9a69528b51799ae55955bcad80d62675
Ben Perlmutter <90647379+mongodben@users.noreply.github.com>
2021-12-08 13:35:26 -0500

    add basic realm todo handling
    all working except for the todos collection listener

--- electron/src/renderer/hooks/useTodos.js
+++ electron/src/renderer/hooks/useTodos.js
@@ -1,25 +1,102 @@
-import { useState } from 'react';
-import { realmApp } from '../realm';
+import { useCallback, useState, useEffect } from 'react';
+import { ipcRenderer } from 'electron';
+import path from 'path';
+import useRealmApp from './useRealmApp';
+import { Todo } from 'schemas';
+
+async function getUserDataPath() {
+  const userDataPath = await ipcRenderer.invoke('get-user-data-path');
+  return userDataPath;
+}
+
+async function openRealm(currentUser, schemas) {
+  const userDataPath = await getUserDataPath();
+  const config = {
+    schema: schemas, // predefined schema
+    path: path.join(userDataPath, 'my.realm'),
+    sync: {
+      user: currentUser, // TODO: this isn't being passed to main process properly.
+      partitionValue: currentUser?.id,
+    },
+  };
+
+  try {
+    const res = await ipcRenderer.invoke('open-realm', config);
+    if (res) {
+      const rendererConfig = { ...config, sync: true };
+      const realm = new Realm(rendererConfig);
+      return realm;
+    } else throw new Error("couldn't communicate with main process");
+  } catch (err) {
+    if (err instanceof Error) {
+      console.error('failed to open realm', err.message);
+    }
+    return null;
+  }
+}
 
 const useTodos = () => {
   // TODO
+  const [realmDb, setRealmDb] = useState(null);
   const [todos, setTodos] = useState([]);
   const [loading, setLoading] = useState(true);
+  const { currentUser } = useRealmApp();
 
-  const fetchUserTodos = async () => {
-    // TODO
-  };
+  const openTodoRealmCb = useCallback(async () => {
+    if (currentUser?.id) {
+      const db = await openRealm(currentUser, [Todo]);
+      setRealmDb(db);
+    }
+  }, [currentUser?.id]);
+
+  // open Todos Realm on component mount and user change
+  // close realm when component dismounts
+  useEffect(() => {
+    openTodoRealmCb();
+
+    return realmDb?.close();
+  }, [currentUser?.id]);
+
+  const fetchUserTodos = useCallback(() => {
+    const todosCollection = realmDb.objects('Todo');
+    setTodos(todosCollection);
+    return todosCollection;
+  }, [realmDb?.schema?.length]);
+
+  useEffect(() => {
+    if (realmDb) {
+      const todosCollection = fetchUserTodos();
+      const todosListener = todosCollection.addListener((collection) => {
+        console.log('collection is', collection);
+        setTodos(collection);
+      });
+      // TODO: was trying to make this work with the `todosListener` variable declared
+      // in the previous line w `todosCollection.removeListener(todosListener)`
+      // but it was giving me an error saying todosListener is undefined. seems to go
+      // against the JS API. verify what's going on here.
+      // the current implementation works fine. just less precise && would be good to figure
+      // out why the other implementation doesn't work
+      return todosCollection?.removeAllListeners();
+    }
+  }, [realmDb?.schema?.length]);
 
   const saveTodo = async (draftTodo) => {
-    // TODO
+    realmDb.write(() => {
+      realmDb.create('Todo', draftTodo, true);
+    });
   };
 
   const toggleTodo = async (todo) => {
-    // TODO
+    realmDb.write(() => {
+      todo.isComplete = true;
+    });
   };
 
   const deleteTodo = async (todo) => {
     // TODO
+    realmDb.write(() => {
+      realmDb.delete(todo);
+    });
   };
 
   // TODO: figure out how to do refresh behavior for todos
