b3f6eb0c704824038d153bd58b8daf0b99e8d855
Ben Perlmutter <90647379+mongodben@users.noreply.github.com>
2022-04-26 15:49:28 -0400

    refactor w animated list widget

--- flutter_todo/lib/components/todo_list.dart
+++ flutter_todo/lib/components/todo_list.dart
@@ -10,16 +10,43 @@ class TodoList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final realm = Provider.of<Realm>(context);
-    final todos = realm.all<Todo>();
+    final initTodos = realm.all<Todo>();
+    final _myListKey = GlobalKey<AnimatedListState>();
+
     return StreamBuilder<RealmResultsChanges<Todo>>(
-        stream: todos.changes,
+        stream: initTodos.changes,
         builder: (context, snapshot) {
           final todos = snapshot.data?.results;
-          return ListView.builder(
-              itemCount: snapshot.data?.results.length ?? 0,
-              itemBuilder: (_, i) {
-                final todo = todos![i];
-                return TodoItem(todo);
+
+          // TODO: i added this summary array so that when the animated list animates
+          // out it creates a new todo for the deleted todo. this was done b/c
+          // before when i was trying to access the todos[deletionIndex] realm
+          // was throwing an error as the todos[deletionIndex] was already deleted
+          // is there a better implementation here?
+          List<String> summaries = [];
+          todos?.forEach((todo) => summaries.add(todo.summary));
+
+          // TODO: right now, it looks like the Todo is deleting from the bottom
+          // of the list regardless of the actual location. how to fix?
+          snapshot.data?.deleted.forEach((deletionIndex) {
+            _myListKey.currentState?.removeItem(
+                deletionIndex,
+                (context, animation) =>
+                    TodoItem(Todo('_', summaries[deletionIndex])));
+          });
+          snapshot.data?.inserted.forEach((insertionIndex) =>
+              _myListKey.currentState?.insertItem(insertionIndex));
+
+          // TODO: how to avoid this?
+          // if i don't have this, then there's an error on page load
+          if (todos?.length == null) {
+            return const SizedBox.shrink();
+          }
+          return AnimatedList(
+              key: _myListKey,
+              initialItemCount: initTodos.length,
+              itemBuilder: (context, index, animation) {
+                return TodoItem(todos![index]);
               });
         });
   }
