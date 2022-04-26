8121ea2cb3ebc5920447d85c24f4240c07ee2318
Ben Perlmutter <90647379+mongodben@users.noreply.github.com>
2022-04-26 12:36:00 -0400

    remove unused file

--- flutter_todo/lib/components/realm_provider.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter_todo/realm/schemas.dart';
-import 'package:realm/realm.dart';
-
-// TODO: is this the best way to create the provider?
-class RealmProvider {
-  Realm realm = Realm(Configuration([Todo.schema]));
-}
