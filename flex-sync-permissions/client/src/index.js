791941673360c31dce729ef6949bbd5f13a807b0
Chris Bush <chris.bush@mongodb.com>
2022-05-05 17:48:31 -0400

    Add collaborators backend/client demo

--- flex-sync-permissions/client/src/index.js
+++ flex-sync-permissions/client/src/index.js
@@ -1,9 +1,8 @@
 import Realm from "realm";
+import { strict as assert } from "assert";
 import * as BSON from "BSON";
 import { appId, baseUrl } from "./realm.json";
 import Timeout from "await-timeout";
-import inquirer from "inquirer";
-import cliProgress from "cli-progress";
 
 const ItemSchema = {
   name: "Item",
@@ -11,33 +10,33 @@ const ItemSchema = {
     _id: "objectId",
     owner_id: "string",
     name: "string",
+    collaborators: "string[]",
   },
   primaryKey: "_id",
 };
 
-const PP = async () => {
+const addCollaboratorsExample = async () => {
   console.log(`Connecting to ${appId}`);
   const app = new Realm.App({ id: appId, baseUrl });
 
   const logIn = async () => {
-    if (app.currentUser !== undefined) {
-      console.log(`Already logged in as user ${app.currentUser.id}`);
-      return app.currentUser;
-    }
     const credentials = Realm.Credentials.anonymous();
     const newUser = await app.logIn(credentials);
     console.log(`Logged in as new user ${newUser.id}`);
     return newUser;
   };
 
-  console.log("Logging in");
-  const user = await logIn();
+  console.log("Logging in as user 1");
+  const user1 = await logIn();
 
-  console.log("Opening synced realm");
+  console.log("Logging in as user 2");
+  const user2 = await logIn();
+
+  console.log("Opening synced realm for user2");
   const realm = await Realm.open({
     schema: [ItemSchema],
     sync: {
-      user,
+      user: user2,
       flexible: true,
       error: (session, error) => {
         console.error("Is connected:", session.isConnected());
@@ -46,42 +45,70 @@ const PP = async () => {
     },
   });
 
-  console.log("Subscribing to my items");
-  const myItems = realm.objects("Item").filtered("owner_id == $0", user.id);
+  console.log("Subscribing to user2's items");
+  const user2Items = realm.objects("Item");
   await realm.subscriptions.update((mutableSubs) => {
-    mutableSubs.add(myItems);
+    mutableSubs.add(user2Items);
   });
 
-  console.log(`My items:`);
-  for (const item of myItems) {
-    console.log(`name: ${item.name}, owner: ${item.owner_id}`);
-  }
-
-  console.log("Creating item");
+  console.log("Creating item with user1 as a collaborator");
   realm.write(() => {
     realm.create("Item", {
       _id: new BSON.ObjectID(),
-      owner_id: user.id,
+      owner_id: user2.id,
       name: "first item",
+      collaborators: [user1.id],
     });
   });
 
-  await Timeout.set(10000);
-
-  console.log("Closing realm");
-  realm.close();
-};
-
-const run = async () => {
-  while (true) {
-    await inquirer.prompt([
-      {
-        type: "list",
-        name: "action",
-        choices: ["log in", "switch user", "create item"],
-      },
-    ]);
+  console.log("Items:");
+  for (const item of user2Items) {
+    console.log(JSON.stringify(item));
   }
+
+  console.log("Uploading all local changes and closing the realm");
+  await realm.syncSession.uploadAllLocalChanges();
+  realm.close();
+
+  console.log("Switching to user1");
+  app.switchUser(user1);
+
+  const realm2 = await Realm.open({
+    schema: [ItemSchema],
+    sync: {
+      user: user1,
+      flexible: true,
+      error: (session, error) => {
+        console.error("Is connected:", session.isConnected());
+        console.error("Error:", error);
+      },
+    },
+  });
+
+  console.log("Subscribing to user1's items");
+  const user1Items = realm2.objects("Item");
+  await realm2.subscriptions.update((mutableSubs) => {
+    mutableSubs.add(user1Items);
+  });
+
+  console.log("Items:");
+  for (const item of user1Items) {
+    console.log(JSON.stringify(item));
+  }
+
+  console.log("Making an edit");
+  realm2.write(() => {
+    const item = user1Items[0];
+    assert("It does not belong to user2", item.owner_id !== user1.id);
+    item.name = "edited successfully!";
+  });
+
+  console.log("Items:");
+  for (const item of user1Items) {
+    console.log(JSON.stringify(item));
+  }
+
+  realm2.close();
 };
 
-run().then(() => process.exit(0));
+addCollaboratorsExample().then(() => process.exit(0));
