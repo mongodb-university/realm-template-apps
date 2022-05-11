315a0f5c9d75ea926408e0562736aa2bc69c8693
Ben Perlmutter <90647379+mongodben@users.noreply.github.com>
2022-05-11 13:41:00 -0400

    Offline Flutter todo app (#53)
    * add flutter_todo app draft
    
    * working realm crud w/o reload of list
    
    * clean up project
    
    * working change listener
    
    * remove unused code
    
    * Some suggestions..
    
    * remove unused file
    
    * rename variable for clarity
    
    * add slidable view
    
    * make cards slidable w checkbox
    
    * remove unnec comment
    
    * refactor todo item card as separate file
    
    * refactor w animated list widget
    
    * fix form modal display on keyboard
    
    * Add transition effects
    
    * fix error deleting last item of list
    
    * Fix deletion issue, by introducing a immutable view model
    
    * Fix missue of ValueKey
    
    * upgrade flutter and remove unnec folders
    
    * update app name
    
    * remove redundant deps
    
    * Enable desktop platforms
    
    * update readme and redownload deps
    
    * ready project for merge to template apps
    
    * update manifest w flutter local app
    
    * remove unnec realm sync config
    
    Co-authored-by: Kasper Overgård Nielsen <kasper@byolimit.com>

---
+++ flutter-local/lib/components/todo_item.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_todo/components/todo_list.dart';
+import 'package:provider/provider.dart';
+import 'package:realm/realm.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
+import 'modify_todo.dart';
+
+class TodoItem extends StatelessWidget {
+  final TodoViewModel viewModel;
+  final Animation<double> animation;
+
+  const TodoItem(this.viewModel, this.animation, {Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final realm = Provider.of<Realm>(context);
+    void deleteItem() {
+      realm.write(() {
+        realm.delete(viewModel.todo);
+      });
+    }
+
+    return FadeTransition(
+      key: key ?? ObjectKey(viewModel),
+      opacity: animation,
+      child: SizeTransition(
+        sizeFactor: animation,
+        child: AnimatedSwitcher(
+          duration: const Duration(milliseconds: 300),
+          child: Slidable(
+            endActionPane: ActionPane(
+              motion: const ScrollMotion(),
+              children: [
+                SlidableAction(
+                  onPressed: (BuildContext context) {
+                    showModifyTodoModal(context, viewModel.todo);
+                  },
+                  flex: 2,
+                  backgroundColor: Color(Colors.blue[500].hashCode),
+                  foregroundColor: Colors.white,
+                  icon: Icons.edit,
+                  label: 'Change',
+                ),
+                SlidableAction(
+                  onPressed: (BuildContext context) {
+                    deleteItem();
+                  },
+                  flex: 2,
+                  backgroundColor: Color(Colors.red[600].hashCode),
+                  foregroundColor: Colors.white,
+                  icon: Icons.delete_forever,
+                  label: 'Delete',
+                )
+              ],
+            ),
+            child: Card(
+              child: ListTile(
+                title: Text(viewModel.summary),
+                subtitle: Text(viewModel.isComplete ? 'Completed' : 'Incomplete'),
+                leading: _CompleteCheckbox(viewModel),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _CompleteCheckbox extends StatelessWidget {
+  final TodoViewModel viewModel;
+  const _CompleteCheckbox(this.viewModel, {Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    final realm = Provider.of<Realm>(context);
+
+    Color getColor(Set<MaterialState> states) {
+      const Set<MaterialState> interactiveStates = <MaterialState>{
+        MaterialState.pressed,
+        MaterialState.hovered,
+        MaterialState.focused,
+      };
+      if (states.any(interactiveStates.contains)) {
+        return Colors.blue;
+      }
+      return Colors.blue;
+    }
+
+    return Checkbox(
+      checkColor: Colors.white,
+      fillColor: MaterialStateProperty.resolveWith(getColor),
+      value: viewModel.isComplete,
+      onChanged: (bool? value) {
+        realm.write(() {
+          viewModel.todo.isComplete = value ?? false;
+        });
+      },
+    );
+  }
+}
---
+++ flutter-local/lib/components/todo_list.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:realm/realm.dart';
+import 'todo_item.dart';
+import 'package:flutter_todo/realm/schemas.dart';
+
+const _animationDuration = Duration(milliseconds: 300);
+
+class TodoViewModel {
+  final String id;
+  final bool isComplete;
+  final String summary;
+  final Todo todo;
+
+  const TodoViewModel._(this.id, this.isComplete, this.summary, this.todo);
+  TodoViewModel(Todo todo) : this._(todo.id, todo.isComplete, todo.summary, todo);
+}
+
+class TodoList extends StatefulWidget {
+  const TodoList({Key? key}) : super(key: key);
+
+  @override
+  State<TodoList> createState() => _TodoListState();
+}
+
+class _TodoListState extends State<TodoList> {
+  final _todoViewModels = <TodoViewModel>[];
+  final _myListKey = GlobalKey<AnimatedListState>();
+
+  @override
+  Widget build(BuildContext context) {
+    final realm = Provider.of<Realm>(context);
+    final stream = realm.all<Todo>().changes;
+
+    return StreamBuilder<RealmResultsChanges<Todo>>(
+        stream: stream,
+        builder: (context, snapshot) {
+          final data = snapshot.data;
+          if (data == null) {
+            // While we wait for data to load..
+            return Container(
+              padding: const EdgeInsets.only(top: 25),
+              child: const Center(child: Text("No Todos yet!")),
+            );
+          }
+
+          final todos = data.results;
+
+          // Handle deletions. These are handles first, as indexes refer to the old collection
+          for (final deletionIndex in data.deleted) {
+            final toDie = _todoViewModels.removeAt(deletionIndex); // update view model collection
+            _myListKey.currentState?.removeItem(deletionIndex, (context, animation) {
+              return TodoItem(toDie, animation);
+            });
+          }
+
+          // Handle inserts
+          for (final insertionIndex in data.inserted) {
+            _todoViewModels.insert(insertionIndex, TodoViewModel(todos[insertionIndex]));
+            _myListKey.currentState?.insertItem(insertionIndex);
+          }
+
+          // Handle modifications
+          for (final modifiedIndex in data.modified) {
+            _todoViewModels[modifiedIndex] = TodoViewModel(todos[modifiedIndex]);
+          }
+
+          // Handle initialization (or any mismatch really, but that shouldn't happen)
+          if (todos.length != _todoViewModels.length) {
+            _todoViewModels.insertAll(0, todos.map(TodoViewModel.new));
+            _todoViewModels.length = todos.length;
+          }
+
+          return AnimatedList(
+              key: _myListKey,
+              initialItemCount: todos.length,
+              itemBuilder: (context, index, animation) {
+                return TodoItem(_todoViewModels[index], animation);
+              });
+        });
+  }
+}
