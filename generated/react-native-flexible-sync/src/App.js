145a5e8904ac0b18b1549395e2458adea6efa4f7
Mohammad Hunan Chughtai <mohammad.hunan@10gen.com>
2022-05-23 12:17:29 -0400

    Convert the Partition Based Sync React Native Template Application to both a PBS and Flexible Sync Template Application (#56)
    * update schema
    
    * upgraded realm + added appwrapper for realm/react
    
    * removed unneeded realmApp + fixed Task Schema to work w/ @realm/react
    
    * use hooks in views components
    
    * added generated react-native flex sync & pbs templates
    
    * fixed manifest file to use generated-react-native-pbs folder
    
    Co-authored-by: Chris Bush <chris.bush@mongodb.com>

---
+++ generated/react-native-flexible-sync/src/App.js
@@ -0,0 +1,79 @@
+import React from 'react';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
+import {StyleSheet, Text, View, ActivityIndicator} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import {AppProvider, UserProvider, useUser} from '@realm/react';
+
+import {LogoutButton} from './LogoutButton';
+import {WelcomeView} from './WelcomeView';
+import {TasksView} from './TasksView';
+import TaskContext from './TaskSchema';
+import {appId, baseUrl} from '../realm';
+
+const Stack = createStackNavigator();
+const {RealmProvider} = TaskContext;
+
+const AppWrapper = () => {
+  return (
+    <AppProvider id={appId} baseUrl={baseUrl}>
+      <UserProvider fallback={WelcomeView}>
+        <App />
+      </UserProvider>
+    </AppProvider>
+  );
+};
+
+const App = () => {
+  return (
+    <>
+      {/* After login, user will be automatically populated in realm configuration */}
+      <RealmProvider
+        sync={{flexible: true}}
+        fallback={() => (
+          <View style={styles.activityContainer}>
+            <ActivityIndicator size="large" />
+          </View>
+        )}>
+        <SafeAreaProvider>
+          <NavigationContainer>
+            <Stack.Navigator>
+              <Stack.Screen
+                name="Tasks"
+                component={TasksView}
+                options={{
+                  headerLeft: () => {
+                    return <LogoutButton />;
+                  },
+                }}
+              />
+            </Stack.Navigator>
+          </NavigationContainer>
+          <View style={styles.footer}>
+            <Text style={styles.footerText}>
+              Built with the MongoDB Realm Sync Template
+            </Text>
+          </View>
+        </SafeAreaProvider>
+      </RealmProvider>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  footerText: {
+    fontSize: 10,
+    textAlign: 'center',
+  },
+  footer: {
+    margin: 40,
+  },
+  activityContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    padding: 10,
+  },
+});
+
+export default AppWrapper;
