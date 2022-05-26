a508e6ad1fdb32a124b6837ee57d1256df4683de
Dachary Carey <dachary.carey@mongodb.com>
2022-05-26 14:12:14 -0400

    Update app to use Flexible Sync

--- swiftui/App/Views/ContentView.swift
+++ swiftui/App/Views/ContentView.swift
@@ -6,9 +6,14 @@ struct ContentView: View {
     
     var body: some View {
         if let user = app.currentUser {
+            // :state-start: partition-based-sync
             // If there is a logged in user, pass the user ID as the
             // partitionValue to the view that opens a realm.
             OpenRealmView().environment(\.partitionValue, user.id)
+            // :state-end:
+            // :state-uncomment-start: flexible-sync
+            // OpenRealmView()
+            // :state-uncomment-end:
         } else {
             // If there is no user logged in, show the login view.
             LoginView()
--- swiftui/App/Views/OpenRealmView.swift
+++ swiftui/App/Views/OpenRealmView.swift
@@ -1,11 +1,27 @@
 import SwiftUI
 import RealmSwift
 
+// :state-uncomment-start: flexible-sync
+// enum SubscriptionState {
+//     case initial
+//     case completed
+// }
+// :state-uncomment-end:
+
 /// Called when login completes. Opens the realm asynchronously and navigates to the Todos screen.
 struct OpenRealmView: View {
+    // :state-uncomment-start: flexible-sync
+    // let user = realmApp.currentUser
+    // @State var subscriptionState: SubscriptionState = .initial
+    // :state-uncomment-end:
+    // :state-start: partition-based-sync
     // By leaving the `partitionValue` an empty string, we use the
     // `.environment(\.partitionValue, user.id)` passed in from the `ContentView`
     @AsyncOpen(appId: theAppConfig.appId, partitionValue: "", timeout: 4000) var asyncOpen
+    // :state-end:
+    // :state-uncomment-start: flexible-sync
+    // @AsyncOpen(appId: testAppId, timeout: 2000) var asyncOpen
+    // :state-uncomment-end:
        
     var body: some View {
        switch asyncOpen {
@@ -20,8 +36,35 @@ struct OpenRealmView: View {
        // The realm has been opened and is ready for use.
        // Show the Todos view.
        case .open(let realm):
+           // :state-start: partition-based-sync
            TodosView(leadingBarButton: AnyView(LogoutButton()))
-                .environment(\.realm, realm)
+               .environment(\.realm, realm)
+           // :state-end:
+           // :state-uncomment-start: flexible-sync
+           // switch subscriptionState {
+           // case .initial:
+           //     ProgressView("Subscribing to Query")
+           //         .onAppear {
+           //             Task {
+           //                 do {
+           //                     let subs = realm.subscriptions
+           //                     if subs.count == 0 {
+           //                         try await subs.write {
+           //                             subs.append(QuerySubscription<Todo>(name: "user_tasks") {
+           //                                 $0.owner_id == user!.id
+           //                             })
+           //                         }
+           //
+           //                     }
+           //                     subscriptionState = .completed
+           //                 }
+           //             }
+           //         }
+           // case .completed:
+           //     TodosView(leadingBarButton: AnyView(LogoutButton()))
+           //          .environment(\.realm, realm)
+           // }
+           // :state-end:
        // The realm is currently being downloaded from the server.
        // Show a progress view.
        case .progress(let progress):
