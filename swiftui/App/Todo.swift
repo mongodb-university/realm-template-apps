5fff2cbed70aa57fae4a6a2187b53425bc656d8f
Dachary Carey <dachary.carey@mongodb.com>
2022-06-03 16:33:55 -0400

    Fix missing opening colon to generate appropriate states

--- swiftui/App/Todo.swift
+++ swiftui/App/Todo.swift
@@ -4,7 +4,7 @@ class Todo: Object, ObjectKeyIdentifiable {
     @Persisted(primaryKey: true) var _id: ObjectId
     @Persisted var isComplete = false
     @Persisted var summary: String
-    // state-start: flexible-sync
+    // :state-start: flexible-sync
     @Persisted var owner_id: String
-    // state-end:
+    // :state-end:
 }
