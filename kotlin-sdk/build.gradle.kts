c4e982adbb998662cf13129e3881c34f8310e6aa
nathan-contino-mongo <nathan.contino@mongodb.com>
2022-06-06 15:13:20 -0400

    Kotlin SDK Flexible Sync Template App

---
+++ kotlin-sdk/build.gradle.kts
@@ -0,0 +1,25 @@
+buildscript {
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
+        maven("https://oss.sonatype.org/content/repositories/snapshots")
+    }
+    dependencies {
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10")
+        classpath("com.android.tools.build:gradle:7.2.0")
+        classpath("io.realm.kotlin:gradle-plugin:0.12.0-SNAPSHOT") // DON'T FORGET TO UPDATE VERSION IN SHARED/ANDROID GRADLE
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://oss.sonatype.org/content/repositories/snapshots")
+    }
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
+}
