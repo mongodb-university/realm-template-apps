04b277fe8b086e794c6becf85b80957a9dae35a5
nate contino <nathan.contino@mongodb.com>
2022-06-07 08:15:00 -0400

    Kotlin 1.0.0 (#76)

--- kotlin-sdk/build.gradle.kts
+++ kotlin-sdk/build.gradle.kts
@@ -3,12 +3,11 @@ buildscript {
         gradlePluginPortal()
         google()
         mavenCentral()
-        maven("https://oss.sonatype.org/content/repositories/snapshots")
     }
     dependencies {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10")
         classpath("com.android.tools.build:gradle:7.2.0")
-        classpath("io.realm.kotlin:gradle-plugin:1.0.0-SNAPSHOT") // DON'T FORGET TO UPDATE VERSION IN APP MODULE GRADLE
+        classpath("io.realm.kotlin:gradle-plugin:1.0.0") // DON'T FORGET TO UPDATE VERSION IN APP MODULE GRADLE
     }
 }
 
@@ -16,7 +15,6 @@ allprojects {
     repositories {
         google()
         mavenCentral()
-        maven("https://oss.sonatype.org/content/repositories/snapshots")
     }
 }
 
