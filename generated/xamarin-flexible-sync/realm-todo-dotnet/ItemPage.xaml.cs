48685d97c8f13d551a5e186b9ec72ac3502685f6
Caleb Thompson <caleb.thompson@mongodb.com>
2022-08-01 15:52:51 -0700

    update dialog to support multiple fields -- generated

--- generated/xamarin-flexible-sync/realm-todo-dotnet/ItemPage.xaml.cs
+++ generated/xamarin-flexible-sync/realm-todo-dotnet/ItemPage.xaml.cs
@@ -6,6 +6,8 @@ using Xamarin.Forms;
 using Realms.Sync;
 using System.Collections.Generic;
 using Realms.Sync.Exceptions;
+using Rg.Plugins.Popup.Contracts;
+using Rg.Plugins.Popup.Services;
 
 namespace RealmTemplateApp
 {
@@ -14,6 +16,8 @@ namespace RealmTemplateApp
         private Realm _itemsRealm;
         private User _user;
         private IEnumerable<Item> _items;
+        private IPopupNavigation _popup { get; set; }
+        private NewItemPopup _modalPage;
 
         public ItemPage()
         {
@@ -26,6 +30,8 @@ namespace RealmTemplateApp
             {
                 //handle errors here
             };
+            _popup = PopupNavigation.Instance;
+            _modalPage = new NewItemPopup();
         }
 
         private void AddSubscriptionsToRealm()
@@ -50,6 +56,13 @@ namespace RealmTemplateApp
             {
                 await DisplayAlert("Error Fetching Items", ex.Message, "OK");
             }
+            _popup.Popped += Popup_Popped;
+        }
+
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
+            _popup.Popped += Popup_Popped;
         }
 
         private void SetUpItemsList()
@@ -64,17 +77,19 @@ namespace RealmTemplateApp
 
         private async void New_Button_Clicked(object sender, EventArgs e)
         {
-            string result = await DisplayPromptAsync("New Item", "Enter the Item Name");
+            await _popup.PushAsync(_modalPage);
+        }
 
-            if (result == null)
-            {
-                return;
-            }
+        private async void Popup_Popped(object sender, Rg.Plugins.Popup.Events.PopupNavigationEventArgs e)
+        {
+            var popup = (NewItemPopup)e.Page;
+
+            if (!popup.IsOK) return;
 
             var newItem = new Item()
             {
                 OwnerId = user.Id.ToString(),
-                Summary = result,
+                Summary = popup.ItemName,
                 IsComplete = false
             };
 
@@ -82,6 +97,7 @@ namespace RealmTemplateApp
             {
                 _itemsRealm.Add(newItem);
             });
+
         }
 
         private async void Logout_Clicked(object sender, EventArgs e)
