58b4ba03134079c97287702b6cbf615631d400a0
Desislava Stefanova <dst.stefanova@gmail.com>
2022-12-02 21:39:45 +0200

    Code review changes

--- v2/flutter_todo/lib/screens/homepage.dart
+++ v2/flutter_todo/lib/screens/homepage.dart
@@ -4,6 +4,7 @@ import 'package:flutter_todo/components/todo_list.dart';
 import 'package:flutter_todo/components/create_item.dart';
 import 'package:flutter_todo/components/app_bar.dart';
 import 'package:flutter_todo/realm/realm_services.dart';
+
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
