dbfbc1c0708fe217132d5a77a29a0812eb619f1f
Ferdinando Papale <>
2022-12-07 21:53:04 +0100

    Corrected sync change

--- maui/RealmTodo/ViewModels/ItemsViewModel.cs
+++ maui/RealmTodo/ViewModels/ItemsViewModel.cs
@@ -4,6 +4,7 @@ using CommunityToolkit.Mvvm.Input;
 using RealmTodo.Models;
 using RealmTodo.Services;
 using Realms;
+using Realms.Sync;
 
 namespace RealmTodo.ViewModels
 {
@@ -33,12 +34,26 @@ namespace RealmTodo.ViewModels
             {
                 await ChangeSubscription(SubscriptionType.Mine);
             }
+            else
+            {
+                var activeSubscription = realm.Subscriptions.First();
+                if (activeSubscription.Name == "all")
+                {
+                    IsShowAllTasks = true;
+                }
+                else
+                {
+                    IsShowAllTasks = false;
+                }
+            }
         }
 
         [RelayCommand]
         public async Task Logout()
         {
+            IsBusy = true;
             await RealmService.App.CurrentUser.LogOutAsync();
+            IsBusy = false;
 
             await Shell.Current.GoToAsync($"//login");
         }
@@ -115,22 +130,33 @@ namespace RealmTodo.ViewModels
 
         private async Task ChangeSubscription(SubscriptionType subType)
         {
+            var activeSubscription = realm.Subscriptions.FirstOrDefault();
+
+            if (activeSubscription?.Name == "all" && subType == SubscriptionType.All
+                || activeSubscription?.Name == "mine" && subType == SubscriptionType.Mine)
+            {
+                return;
+            }
+
             realm.Subscriptions.Update(() =>
             {
                 realm.Subscriptions.RemoveAll(true);
 
                 IQueryable<Item> query = null;
+                string queryName = null;
 
                 if (subType == SubscriptionType.Mine)
                 {
                     query = realm.All<Item>().Where(i => i.OwnerId == currentUserId);
+                    queryName = "mine";
                 }
                 else
                 {
                     query = realm.All<Item>();
+                    queryName = "all";
                 }
 
-                realm.Subscriptions.Add(query);
+                realm.Subscriptions.Add(query, new SubscriptionOptions { Name = queryName });
             });
 
             await realm.Subscriptions.WaitForSynchronizationAsync();
