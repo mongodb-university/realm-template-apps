d211e88b9b70da713ad5223cb6ef7ddc400367dd
Andrew Meyer <andrew.meyer@mongodb.com>
2022-12-13 08:56:02 +0100

    Fix Logout
    There appears to be a conflict with the connection notification listener
    and logout.  This was causing the logout method to never resolve, resulting
    in a locked up UI. The listener has been removed to restore functionality.

--- react-native-ts/src/OfflineModeButton.tsx
+++ react-native-ts/src/OfflineModeButton.tsx
@@ -1,8 +1,6 @@
-import React, {useEffect, useReducer} from 'react';
+import React, {useState} from 'react';
 import {Pressable, StyleSheet} from 'react-native';
 import {Icon} from 'react-native-elements';
-import {ConnectionNotificationCallback} from 'realm';
-import {useRerender} from './Hooks';
 import {realmContext} from './RealmContext';
 
 const {useRealm} = realmContext;
@@ -10,26 +8,17 @@ const {useRealm} = realmContext;
 export function OfflineModeButton() {
   const realm = useRealm();
 
-  const rerender = useRerender();
-
-  // Call the rerender function when the connection state changes
-  useEffect(() => {
-    const notificationCallback: ConnectionNotificationCallback = newState => {
-      rerender();
-    };
-    realm.syncSession?.addConnectionNotification(notificationCallback);
-    return () => {
-      realm.syncSession?.removeConnectionNotification(notificationCallback);
-    };
-  }, [realm, rerender]);
+  const [pauseSync, togglePauseSync] = useState(false);
 
   return (
     <Pressable
       onPress={() => {
-        if (realm.syncSession?.state === 'active') {
+        if (!pauseSync && realm.syncSession?.state === 'active') {
           realm.syncSession.pause();
-        } else if (realm.syncSession?.state === 'inactive') {
+          togglePauseSync(true);
+        } else if (pauseSync && realm.syncSession?.state === 'inactive') {
           realm.syncSession.resume();
+          togglePauseSync(false);
         }
       }}>
       <Icon
