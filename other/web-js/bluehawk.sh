c64ff31c098aefb036337a47c007295fff97b485
Chris Bush <chris.bush@mongodb.com>
2022-12-12 23:14:03 -0500

    Copy template realm config files to generated apps

--- other/web-js/bluehawk.sh
+++ other/web-js/bluehawk.sh
@@ -1,6 +1,8 @@
 #!/bin/bash -ex
+
 pushd "$(dirname "$0")"
 for state in prod-mql prod-graphql; do
   npx bluehawk copy --state $state -o generated/$state client/;
+  cp template-realm.json generated/$state/src/realm.json
 done
 popd
