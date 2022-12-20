7c273877f57de717edf2df4d57ccee959bf86bb3
Ferdinando Papale <4850119+papafe@users.noreply.github.com>
2022-12-20 18:55:35 +0100

    MAUI template app (#117)
    The code is ready for review, but there is a strange behaviour with sync
    due to https://github.com/realm/realm-dotnet/issues/3128. This can cause
    some error loops on iOS in some specific situations
    
    Co-authored-by: Ferdinando Papale <>
    Co-authored-by: Chris Bush <chris.bush@10gen.com>
    Co-authored-by: MongoCaleb <32645888+MongoCaleb@users.noreply.github.com>

---
+++ sync-todo/v2/client/maui/RealmTodo/ViewModels/BaseViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace RealmTodo.ViewModels
+{
+    public partial class BaseViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        protected bool isBusy;
+
+        protected Action currentDismissAction;
+
+        partial void OnIsBusyChanged(bool value)
+        {
+            if (value)
+            {
+                currentDismissAction = Services.DialogService.ShowActivityIndicator();
+            }
+            else
+            {
+                currentDismissAction?.Invoke();
+                currentDismissAction = null;
+            }
+        }
+
+    }
+}
+
---
+++ sync-todo/v2/client/maui/RealmTodo/Views/LoginPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace RealmTodo.Views;
+
+public partial class LoginPage : ContentPage
+{
+    public LoginPage()
+    {
+        InitializeComponent();
+    }
+}
