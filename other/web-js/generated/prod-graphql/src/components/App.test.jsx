f9aeff8b103e272c2b711d5a5b115d67f778b062
Nick Larew <nick.larew@mongodb.com>
2023-04-13 12:04:54 -0400

    Move integration test to a shell file

--- other/web-js/generated/prod-graphql/src/components/App.test.jsx
+++ other/web-js/generated/prod-graphql/src/components/App.test.jsx
@@ -21,52 +21,56 @@ function setup(jsx) {
 
 test("initially renders the login/signup page", () => {
   setup(<App />);
-  expect(screen.getByText(/Welcome!/i)).toBeInTheDocument();
+  expect(screen.queryByText(/Welcome!/i)).toBeInTheDocument();
 });
 
 test("allows you to sign up for a new account", async () => {
   const { user } = setup(<App />);
-  await user.click(screen.getByText(/Sign up for an account/i));
-  await user.type(screen.getByLabelText("Email Address"), TEST_USERNAME);
-  await user.type(screen.getByLabelText("Password"), "aa");
-  await user.click(screen.getByTestId("submit-button"));
+  await user.click(screen.queryByText(/Sign up for an account/i));
+  await user.type(screen.queryByLabelText("Email Address"), TEST_USERNAME);
+  await user.type(screen.queryByLabelText("Password"), "aa");
+  await user.click(screen.queryByTestId("submit-button"));
   await waitFor(
     () => {
       expect(
-        screen.getByText(/Password must be between 6 and 128 characters./i)
+        screen.queryByText(/Password must be between 6 and 128 characters./i)
       ).toBeInTheDocument();
     },
     { timeout: 5000 }
   );
-  await user.clear(screen.getByLabelText("Password"));
-  await user.type(screen.getByLabelText("Password"), TEST_PASSWORD);
-  await user.click(screen.getByTestId("submit-button"));
-  await wait(10)
+  await user.clear(screen.queryByLabelText("Password"));
+  await user.type(screen.queryByLabelText("Password"), TEST_PASSWORD);
+  await user.click(screen.queryByTestId("submit-button"));
+  await wait(10);
   await waitFor(
     () => {
-      expect(screen.getByText(/You have 0 To-Do Items/i)).toBeInTheDocument();
+      expect(screen.queryByText(/You have 0 To-Do Items/i)).toBeInTheDocument();
+    },
+    { timeout: 7000 }
+  );
+  await user.click(screen.queryByText(/Log out/i));
+  await waitFor(
+    () => {
+      expect(screen.queryByText(/Welcome!/i)).toBeInTheDocument();
     },
     { timeout: 4000 }
   );
-  await user.click(screen.getByText(/Log out/i));
-  await waitFor(() => {
-    expect(screen.getByText(/Welcome!/i)).toBeInTheDocument();
-  });
-}, 10000);
+}, 12000);
 
 test("allows you to log in with an existing account", async () => {
   const { user } = setup(<App />);
-  if (screen.queryByText(/Log out/i)) {
-    await user.click(screen.getByText(/Log out/i));
+  const logOutButton = screen.queryByText(/Log out/i);
+  if (logOutButton) {
+    await user.click(logOutButton);
   }
-  expect(screen.getByText(/Welcome!/i)).toBeInTheDocument();
-  await user.type(screen.getByLabelText("Email Address"), TEST_USERNAME);
-  await user.type(screen.getByLabelText("Password"), TEST_PASSWORD);
-  await user.click(screen.getByTestId("submit-button"));
+  expect(screen.queryByText(/Welcome!/i)).toBeInTheDocument();
+  await user.type(screen.queryByLabelText("Email Address"), TEST_USERNAME);
+  await user.type(screen.queryByLabelText("Password"), TEST_PASSWORD);
+  await user.click(screen.queryByTestId("submit-button"));
   await waitFor(
     () => {
       expect(
-        screen.getByText(/You have (.+) To-Do Item(s?)/i)
+        screen.queryByText(/You have (.+) To-Do Item(s?)/i)
       ).toBeInTheDocument();
     },
     { timeout: 5000 }
@@ -75,41 +79,44 @@ test("allows you to log in with an existing account", async () => {
 
 test("allows you to CRUD to-do items", async () => {
   const { user } = setup(<App />);
-  await waitFor(() => {
-    expect(screen.getByText(/You have 0 To-Do Items/i)).toBeInTheDocument();
-  }, { timeout: 4000 });
+  await waitFor(
+    () => {
+      expect(screen.queryByText(/You have 0 To-Do Items/i)).toBeInTheDocument();
+    },
+    { timeout: 4000 }
+  );
   // Add the first To-Do
-  await user.click(screen.getByText(/Add To-Do/i));
+  await user.click(screen.queryByText(/Add To-Do/i));
   await user.type(
-    screen.getByPlaceholderText("What needs doing?"),
+    screen.queryByPlaceholderText("What needs doing?"),
     "Do the dishes"
   );
-  await user.click(screen.getByText(/Save/i));
+  await user.click(screen.queryByText(/Save/i));
   await wait(10);
   await waitFor(
     () => {
       expect(
         screen.queryByPlaceholderText("What needs doing?")
       ).not.toBeInTheDocument();
-      expect(screen.getByText(/You have 1 To-Do Item/i)).toBeInTheDocument();
-      expect(screen.getByText(/Do the dishes/i)).toBeInTheDocument();
+      expect(screen.queryByText(/You have 1 To-Do Item/i)).toBeInTheDocument();
+      expect(screen.queryByText(/Do the dishes/i)).toBeInTheDocument();
     },
     { timeout: 10000 }
   );
   // Add a second To-Do
-  await user.click(screen.getByText(/Add To-Do/i));
+  await user.click(screen.queryByText(/Add To-Do/i));
   await user.type(
-    screen.getByPlaceholderText("What needs doing?"),
+    screen.queryByPlaceholderText("What needs doing?"),
     "Do the laundry"
   );
-  await user.click(screen.getByText(/Save/i));
-  await wait(10)
+  await user.click(screen.queryByText(/Save/i));
+  await wait(10);
   await waitFor(
     () => {
       expect(
         screen.queryByPlaceholderText("What needs doing?")
       ).not.toBeInTheDocument();
-      expect(screen.getByText(/You have 2 To-Do Items/i)).toBeInTheDocument();
+      expect(screen.queryByText(/You have 2 To-Do Items/i)).toBeInTheDocument();
     },
     { timeout: 8000 }
   );
@@ -131,11 +138,11 @@ test("allows you to CRUD to-do items", async () => {
   const deleteButtons = screen.getAllByTestId("todo-delete-button");
   expect(deleteButtons.length).toBe(2);
   await user.click(deleteButtons[0]);
-  await wait(10)
+  await wait(10);
   await waitFor(
     () => {
-      expect(screen.getByText(/You have 1 To-Do Item/i)).toBeInTheDocument();
-      expect(screen.getByText(/Do the laundry/i)).toBeInTheDocument();
+      expect(screen.queryByText(/You have 1 To-Do Item/i)).toBeInTheDocument();
+      expect(screen.queryByText(/Do the laundry/i)).toBeInTheDocument();
     },
     { timeout: 10000 }
   );
@@ -146,7 +153,10 @@ test("allows you to log out", async () => {
   const logoutButton = screen.queryByText(/Log out/i);
   expect(logoutButton).toBeInTheDocument();
   await user.click(logoutButton);
-  await waitFor(() => {
-    expect(screen.getByText(/Welcome!/i)).toBeInTheDocument();
-  });
+  await waitFor(
+    () => {
+      expect(screen.queryByText(/Welcome!/i)).toBeInTheDocument();
+    },
+    { timeout: 4000 }
+  );
 });
