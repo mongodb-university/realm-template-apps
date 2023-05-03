b03ecf8ad3188838f846e0a4dde05ab5dfe858c1
Dachary <dachary.carey@mongodb.com>
2023-05-03 16:53:44 -0400

    Bump the Kotlin SDK version to 1.8.0 (#150)
    - Bump the SDK version to 1.8.0
    - Update the deprecated ObjectId
    - Add the generated RN package.json

--- sync-todo/v2/generated/kotlin-sdk/app/build.gradle.kts
+++ sync-todo/v2/generated/kotlin-sdk/app/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("com.android.application")
     kotlin("android")
-    id("io.realm.kotlin") version "1.4.0"
+    id("io.realm.kotlin") version "1.8.0"
 }
 
 android {
@@ -43,5 +43,5 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.5.1")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
-    implementation("io.realm.kotlin:library-sync:1.4.0") // DON'T FORGET TO UPDATE VERSION IN PROJECT GRADLE
+    implementation("io.realm.kotlin:library-sync:1.8.0") // DON'T FORGET TO UPDATE VERSION IN PROJECT GRADLE
 }
