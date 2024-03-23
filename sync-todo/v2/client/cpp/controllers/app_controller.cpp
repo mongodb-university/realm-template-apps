552d779863d7fa3ea581642e5d0c1f98b4dd9357
dacharyc <dachary.carey@mongodb.com>
2024-03-22 21:23:22 -0400

    Move AppController implementation from hpp to cpp, clean up

--- sync-todo/v2/client/cpp/controllers/app_controller.cpp
+++ sync-todo/v2/client/cpp/controllers/app_controller.cpp
@@ -4,43 +4,64 @@ void AppController::onFrame() {
   _navigation.onFrame();
 }
 
-//AppController::AppController() {
-//
-//  _appState.authManager = std::make_unique<AuthManager>(this);
-//  _appState.errorManager = std::make_unique<ErrorManager>(this);
-//
-//  _errorModal = ftxui::Container::Vertical({
-//    ftxui::Renderer([this] {
-//      return ftxui::text(_appState.errorManager->getError().value());
-//    }),
-//    ftxui::Button("Dismiss", [=] {
-//      _appState.errorManager->clearError();
-//    }),
-//    });
-//
-//  component()->Add(_navigation.component());
-//
-//  if (isUserLoggedIn) {
-//    _navigation.goTo(std::make_unique<HomeController>(&_appState));
-//  } else {
-//    _navigation.goTo(std::make_unique<LoginController>(&_appState));
-//  }
-//}
+AppController::AppController(ftxui::ScreenInteractive *screen) {
+  _appState.screen = screen;
 
-//void AppController::onLoggedIn() override {
-//  _navigation.goTo(std::make_unique<HomeController>(&_appState));
-//}
-//
-//void AppController::onLoggedOut() override {
-//  _navigation.goTo(std::make_unique<LoginController>(&_appState));
-//}
-//
-//void AppController::onError(ErrorManager &error) override {
-//  _errorModal->Detach();
-//  component()->Add(_errorModal);
-//  _errorModal->TakeFocus();
-//}
-//
-//void AppController::onErrorCleared(ErrorManager &error) override {
-//  _errorModal->Detach();
-//}
+  /** Read the contents of the atlasConfig.json to get the metadata for the App Services App.
+   * This path assumes you are running the app from a `/build` directory within this project. If you're
+   * running the app somewhere else, update the path to your atlasConfig.json accordingly. */
+  std::ifstream f("../atlasConfig.json");
+  nlohmann::json data = nlohmann::json::parse(f);
+  auto appConfigMetadata = data.template get<AppConfigMetadata>();
+  f.close();
+
+  auto appConfig = realm::App::configuration {
+      .app_id = appConfigMetadata.appId
+  };
+  _appState.app = std::make_unique<realm::App>(appConfig);
+  _appState.authManager = std::make_unique<AuthManager>(this);
+  _appState.errorManager = std::make_unique<ErrorManager>(this);
+
+  _errorModal = ftxui::Container::Vertical({
+    ftxui::Renderer([this] {
+      return ftxui::text(_appState.errorManager->getError().value());
+    }),
+    ftxui::Button("Dismiss", [=] {
+      _appState.errorManager->clearError();
+    }),
+    });
+
+  component()->Add(_navigation.component());
+
+  if (_appState.app->get_current_user()) {
+    isUserLoggedIn = true;
+  }
+
+  if (isUserLoggedIn) {
+    _navigation.goTo(std::make_unique<HomeController>(&_appState));
+  } else {
+    _navigation.goTo(std::make_unique<LoginController>(&_appState));
+  }
+}
+
+void AppController::onRegisteredAndLoggedIn() {
+  _navigation.goTo(std::make_unique<HomeController>(&_appState));
+}
+
+void AppController::onLoggedIn() {
+  _navigation.goTo(std::make_unique<HomeController>(&_appState));
+}
+
+void AppController::onLoggedOut() {
+  _navigation.goTo(std::make_unique<LoginController>(&_appState));
+}
+
+void AppController::onError(ErrorManager &error) {
+  _errorModal->Detach();
+  component()->Add(_errorModal);
+  _errorModal->TakeFocus();
+}
+
+void AppController::onErrorCleared(ErrorManager &error) {
+  _errorModal->Detach();
+}
--- sync-todo/v2/client/cpp/controllers/app_controller.hpp
+++ sync-todo/v2/client/cpp/controllers/app_controller.hpp
@@ -25,70 +25,18 @@ class AppController final : public Controller, public AuthManager::Delegate, pub
   ftxui::Component _errorModal;
 
  public:
-//  AppController();
-  AppController(ftxui::ScreenInteractive *screen) {
-    _appState.screen = screen;
-
-    /** Read the contents of the atlasConfig.json to get the metadata for the App Services App.
-     * This path assumes you are running the app from a `/build` directory within this project. If you're
-     * running the app somewhere else, update the path to your atlasConfig.json accordingly. */
-    std::ifstream f("../atlasConfig.json");
-    nlohmann::json data = nlohmann::json::parse(f);
-    auto appConfigMetadata = data.template get<AppConfigMetadata>();
-    f.close();
-
-    auto appConfig = realm::App::configuration {
-        .app_id = appConfigMetadata.appId
-    };
-    _appState.app = std::make_unique<realm::App>(appConfig);
-    _appState.authManager = std::make_unique<AuthManager>(this);
-    //_appState.authManager->logIn(_appState.app.get(), "dac", "testing9786");
-    _appState.errorManager = std::make_unique<ErrorManager>(this);
-
-    _errorModal = ftxui::Container::Vertical({
-      ftxui::Renderer([this] {
-        return ftxui::text(_appState.errorManager->getError().value());
-      }),
-      ftxui::Button("Dismiss", [=] {
-        _appState.errorManager->clearError();
-      }),
-      });
-
-    component()->Add(_navigation.component());
-
-    if (_appState.app->get_current_user()) {
-      isUserLoggedIn = true;
-    }
-
-    if (isUserLoggedIn) {
-      _navigation.goTo(std::make_unique<HomeController>(&_appState));
-    } else {
-      _navigation.goTo(std::make_unique<LoginController>(&_appState));
-    }
-  }
+  explicit AppController(ftxui::ScreenInteractive *screen);
 
   void onFrame() override;
 
  private:
-  void onRegisteredAndLoggedIn() override {
-    _navigation.goTo(std::make_unique<HomeController>(&_appState));
-  }
+  void onRegisteredAndLoggedIn() override;
 
-  void onLoggedIn() override {
-    _navigation.goTo(std::make_unique<HomeController>(&_appState));
-  }
+  void onLoggedIn() override;
 
-  void onLoggedOut() override {
-    _navigation.goTo(std::make_unique<LoginController>(&_appState));
-  }
+  void onLoggedOut() override;
 
-  void onError(ErrorManager &error) override {
-    _errorModal->Detach();
-    component()->Add(_errorModal);
-    _errorModal->TakeFocus();
-  }
+  void onError(ErrorManager &error) override;
 
-  void onErrorCleared(ErrorManager &error) override {
-    _errorModal->Detach();
-  }
+  void onErrorCleared(ErrorManager &error) override;
 };
