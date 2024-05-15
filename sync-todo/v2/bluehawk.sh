187f33282a5864dfd921dbce5bf018204ea1d052
dacharyc <dachary.carey@mongodb.com>
2024-05-15 11:57:26 -0400

    Add C++ to the bluehawk copy script now that syntax collisions are fixed

--- sync-todo/v2/bluehawk.sh
+++ sync-todo/v2/bluehawk.sh
@@ -3,14 +3,11 @@
 pushd "$(dirname "$0")"
 
 for client in client/*; do
-  # Can't Bluehawk copy the cpp client because of `:state:` in the code
-  # Can remove if block when this issue is resolved: https://github.com/mongodb-university/Bluehawk/issues/145
-  if [[ "$client" != "client/cpp"* ]]; then
   rm -r generated/$(basename $client)
   npx bluehawk copy -o generated/$(basename $client) $client/;
-  fi
 done
 
+cp template-cpp-atlasConfig.json generated/cpp/atlasConfig.json
 cp template-atlasConfig.json generated/flutter/assets/config/atlasConfig.json
 cp template-atlasConfig.json generated/react-native/atlasConfig.json
 cp template-atlasConfig.plist generated/swiftui/App/atlasConfig.plist
